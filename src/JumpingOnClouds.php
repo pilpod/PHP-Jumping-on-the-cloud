828f87ec09330164c2343296321ff6aec54f3714
pilpod <giacomo.corso@gmail.com>
2021-05-01 20:28:17 +0200

    exercise initialized

---
+++ src/JumpingOnClouds.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class JumpingOnClouds {
+
+    public function jumpingOnClouds($c) {
+        // Write your code here
+    
+    }
+
+}
---
+++ tests/JumpingOnCloudsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class JumpingOnCloudsTest extends TestCase {
+
+    public function test_example()
+    {
+        $this->assertEquals(2, 2);
+    }
+
+}
