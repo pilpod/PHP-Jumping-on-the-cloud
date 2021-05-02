f5119f6757ec17bbea1bbc7d6d9309329315bb1b
pilpod <giacomo.corso@gmail.com>
2021-05-02 15:17:16 +0200

    test without double jump working

--- src/JumpingOnClouds.php
+++ src/JumpingOnClouds.php
@@ -4,9 +4,17 @@ namespace App;
 
 class JumpingOnClouds {
 
-    public function jumpingOnClouds($c) {
-        // Write your code here
-    
+    public function jumpingOnClouds(array $clouds) : int
+    {
+        $jumpsNumb = 0;
+
+        for ($i=1; $i < count($clouds); $i++) { 
+            if($clouds[$i] == 0) {
+                $jumpsNumb++;
+            }
+        }
+
+        return $jumpsNumb;
     }
 
 }
--- tests/JumpingOnCloudsTest.php
+++ tests/JumpingOnCloudsTest.php
@@ -2,13 +2,49 @@
 
 namespace Tests;
 
+use App\JumpingOnClouds;
 use PHPUnit\Framework\TestCase;
 
 class JumpingOnCloudsTest extends TestCase {
 
-    public function test_example()
+    public function test_game_with_two_clouds_without_thunderheads()
     {
-        $this->assertEquals(2, 2);
+        $game = new JumpingOnClouds;
+        $c = [0,0];
+        
+        $jumpsResult = $game->jumpingOnClouds($c);
+
+        $this->assertEquals(1, $jumpsResult);
+    }
+
+    public function test_game_with_two_clouds_with_One_thunderheads()
+    {
+        $game = new JumpingOnClouds;
+        $c = [0,1];
+        
+        $jumpsResult = $game->jumpingOnClouds($c);
+
+        $this->assertEquals(0, $jumpsResult);
+    }
+
+    public function test_game_with_6_clouds_two_thunderheads_without_double_jump()
+    {
+        $game = new JumpingOnClouds;
+        $c = [0,1,0,0,1,0];
+        
+        $jumpsResult = $game->jumpingOnClouds($c);
+
+        $this->assertEquals(3, $jumpsResult);
+    }
+
+    public function test_game_with_7_clouds_two_thunderheads_with_double_jump()
+    {
+        $game = new JumpingOnClouds;
+        $c = [0,1,0,0,0,1,0];
+        
+        $jumpsResult = $game->jumpingOnClouds($c);
+
+        $this->assertEquals(3, $jumpsResult);
     }
 
 }
